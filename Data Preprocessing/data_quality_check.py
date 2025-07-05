3ea23839a679de12ff7dee1dcb2e26a35b85e483
Umair <shaikhumair297@gmail.com>
2025-07-05 16:38:02 +0500

    removed unused dependencies

--- Data Preprocessing/data_quality_check.py
+++ Data Preprocessing/data_quality_check.py
@@ -1,8 +1,6 @@
 # data_quality_check.py
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
 
 # Load raw CSV
 df = pd.read_csv("karachi_weather_apr1_to_current.csv")
